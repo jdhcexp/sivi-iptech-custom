1a3e1770507a3d1dec5797d6bb7a9cbce6d8d09e
Ubuntu <ubuntu@ip-172-31-2-240.us-east-2.compute.internal>
2024-04-09 03:09:54 +0000

    update order_preview_extra

--- order_preview_extra/static/src/doc_upload.js
+++ order_preview_extra/static/src/doc_upload.js
@@ -11,7 +11,7 @@ export class DocUpload extends Component {
     setup() {
         console.log('start');
         document.getElementsByClassName("card-body text-center")[0].style.display='none'
-        this.state = useState({ type: "nat", files:[], ccname:'', ccnameback:'', servname:'', rutname:'', cycname:'', ccrname:'', ccrnameback:'', hidecc:false, hideccback: false, hideserv:false, hiderut:false, hidecyc:false, hideccr:false, hideccrback: false });
+        this.state = useState({ sendEnabled:true,  type: "nat", files:[], ccname:'', ccnameback:'', servname:'', rutname:'', cycname:'', ccrname:'', ccrnameback:'', hidecc:false, hideccback: false, hideserv:false, hiderut:false, hidecyc:false, hideccr:false, hideccrback: false });
 
         /*this.orm = registry.category("services").content.orm;
         debugger;
@@ -22,7 +22,9 @@ export class DocUpload extends Component {
     }
     async uploadDoc() {
         console.log("upload file")
+        debugger;
 
+        this.state.files=[];
 
         if (this.state.type == "nat") {
 
@@ -66,58 +68,73 @@ export class DocUpload extends Component {
         }
         const archivoInput = document.getElementById('formFile');
         const orderId = document.getElementById('sale_order_id').innerText;
+        var validated = true;
+        var msg = "Debe cargar todos los archivos:\n"
         for (const x of this.state.files) {
             const archivo = x.file.files[0];
-            console.log(archivo)
             if (!archivo) {
-                alert('Selecciona un archivo primero.');
-                continue;
+                msg = msg + "- "+x.type+"\n"
+                validated = false;
             }
+        }
+        if(!validated){
+            alert(msg);
+        }
+        else
+        {
+            var sendescount=0;
+            for (const x of this.state.files) {
+                const archivo = x.file.files[0];
 
-            const formData = new FormData();
-            formData.append('archivo', archivo);
-            formData.append('orderId', parseInt(orderId));
-            formData.append('type', x.type);
-            await fetch('https://iptechservice.sivi.app/upload', {
-                method: 'POST',
-                body: formData,
-            })
-                .then(response => response.text())
-                .then(data => {
-                    x.sended = true
-                    if(x.type=='cedula frente'){
-                        this.state.hidecc = true
-                        this.state.ccname = x.file.files[0].name
-                    }
-                    if(x.type=='cedula respaldo'){
-                        this.state.hideccback = true
-                        this.state.ccnameback = x.file.files[0].name
-                    }
-                    if(x.type=='recibo servicio'){
-                        this.state.hideserv = true
-                        this.state.servname = x.file.files[0].name
-                    }
-                    if(x.type=='rut'){
-                        this.state.hiderut = true
-                        this.state.rutname = x.file.files[0].name
-                    }
-                    if(x.type=='camara y comercio'){
-                        this.state.hidecyc = true
-                        this.state.cycname = x.file.files[0].name
-                    }
-                    if(x.type=='cedula representante'){
-                        this.state.hideccr = true
-                        this.state.ccrname = x.file.files[0].name
-                    }
-                    if(x.type=='cedula representante back'){
-                        this.state.hideccrback = true
-                        this.state.ccrnameback = x.file.files[0].name
-                    }
-
+                const formData = new FormData();
+                formData.append('archivo', archivo);
+                formData.append('orderId', parseInt(orderId));
+                formData.append('type', x.type);
+                await fetch('https://iptechservice.sivi.app/upload', {
+                    method: 'POST',
+                    body: formData,
                 })
-                .catch(error => {
-                    console.error('Error al subir archivo:', error);
-                });
+                    .then(response => response.text())
+                    .then(data => {
+                        sendescount++;
+                        x.sended = true
+                        if(x.type=='cedula frente'){
+                            this.state.hidecc = true
+                            this.state.ccname = x.file.files[0].name
+                        }
+                        if(x.type=='cedula respaldo'){
+                            this.state.hideccback = true
+                            this.state.ccnameback = x.file.files[0].name
+                        }
+                        if(x.type=='recibo servicio'){
+                            this.state.hideserv = true
+                            this.state.servname = x.file.files[0].name
+                        }
+                        if(x.type=='rut'){
+                            this.state.hiderut = true
+                            this.state.rutname = x.file.files[0].name
+                        }
+                        if(x.type=='camara y comercio'){
+                            this.state.hidecyc = true
+                            this.state.cycname = x.file.files[0].name
+                        }
+                        if(x.type=='cedula representante frente'){
+                            this.state.hideccr = true
+                            this.state.ccrname = x.file.files[0].name
+                        }
+                        if(x.type=='cedula representante back'){
+                            this.state.hideccrback = true
+                            this.state.ccrnameback = x.file.files[0].name
+                        }
+
+                    })
+                    .catch(error => {
+                        console.error('Error al subir archivo:', error);
+                    });
+            }
+            if(sendescount == this.state.files.length){
+                this.state.sendEnabled = false;
+            }
         }
 
     }
