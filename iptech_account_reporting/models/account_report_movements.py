202957716d6fe093a973bbd35c7d8118ea707d3d
Juan Horta <juanhorta@Juans-Mac-mini.local>
2024-05-19 22:27:30 -0500

    update

--- iptech_account_reporting/models/account_report_movements.py
+++ iptech_account_reporting/models/account_report_movements.py
@@ -21,7 +21,7 @@ class AccountReportMovements(models.Model):
 
     def init(self):
         self._cr.execute("""      
-		DROP VIEW account_sql_report_movemenns;                    
+		DROP VIEW account_sql_report_movements;                    
             CREATE OR REPLACE VIEW account_sql_report_movements AS (
             SELECT row_number() OVER() as id,   
 				'001' as a1,
